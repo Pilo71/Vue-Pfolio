cb9dcc5b2eba6b222ec185f0456fe99e8bd6cf63
pilo1971 <cecile.pilorger@gmail.com>
2024-02-28 17:02:22 +0100

    premier commit

---
+++ src/views/contact.vue
@@ -0,0 +1,57 @@
+<template>
+  <section>
+    <div class="decal2">
+      <h1 class="name3 ">Contact</h1>
+      <h2 class="contact1">Remplissez notre formulaire afin d'établir un premier contact !</h2>
+    </div>
+    <div class="form">
+      <form id="contact" action="">
+
+        <div class="colonnes tipo">
+          <label for="first-name">Nom et Prénom</label>
+          <input class="style" type="text" v-model="form.firstName" name="firstName" id="form.firstName" />
+        </div>
+        <div class="colonnes tipo4">
+          <label for="last-name">objet</label>
+          <input class="style" type="text" v-model="form.objet" name="objet" id="form.objet" />
+        </div>
+
+        <div class="colonnes tipo5">
+          <label for="message">Message</label>
+          <textarea class="style2" type="text" v-model="form.message" name="message" id="form.message"></textarea>
+        </div>
+        <div class="send">
+          <button class="bouton name2 center hover4" @click.prevent="sendData()">envoyer</button>
+        </div>
+        <div class=" send" v-for="form in errors">{{ form }}</div>
+      </form>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "contact",
+  data() {
+    return {
+      errors: [],
+      form: {
+        firstName: null,
+        objet: null,
+        message: null,
+      },
+    };
+  },
+  methods: {
+    sendData() {
+      this.errors = [];
+      if (!this.form.message) {
+        this.errors.push("Tous les champs doivent étre remplis");
+        return false;
+      }
+      this.form = {};
+      alert("données envoyées");
+    }
+  },
+
+};
+</script>
---
+++ src/views/modales.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class=" modale " v-if="revele">
+        <div class="overlay pointeur" v-on:click="toggleModale"></div>
+        <div class="cadre2">
+            <div class="boutonModale pointeur" v-on:click="toggleModale">
+                <h4>X</h4>
+            </div>
+            <h2>C.V en ligne</h2>
+            <div>
+                <p>Date de création : 20/10/2023</p>
+            </div>
+            <div>
+                <p>Technologies utilisées : HTML,CSS</p>
+            </div>
+            <div>
+                <p>Liens : <a class="style" href="https://github.com/Pilo71/Mon-CV.git" target="_blank">
+                        Github<img class="taille" src="../images/github.png" alt=""></a>
+                </p>
+
+            </div>
+        </div>
+    </div>
+    <div class=" modale " v-if="revele2">
+        <div class="overlay pointeur" v-on:click="toggleModale2"></div>
+        <div class="cadre2">
+            <div class="boutonModale pointeur" v-on:click="toggleModale2">
+                <h4>X</h4>
+            </div>
+            <h2>Cahier des charges</h2>
+            <div>
+                <p>Date de création : 20/11/2023</p>
+            </div>
+            <div>
+                <p>Technologies utilisées : Photoshop</p>
+            </div>
+            <div>
+
+                <p>Liens : <a class="style" href="/src/assets/Classeur Cahier des charges.pdf" target="_blank">
+                        PDF<img class="taille " src="../images/pdf.png" alt=""></a>
+                </p>
+
+
+            </div>
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "modale ",
+    name: "modale2",
+
+    props: ['revele', 'toggleModale', 'revele2', 'toggleModale2'],
+
+
+
+}
+
+</script>
+<style scoped>
+.overlay {
+    background: rgba(0, 0, 0, 0.6);
+
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    right: 0;
+    left: 0;
+    top: 0;
+    bottom: 0;
+}
+
+
+
+.boutonModale {
+    background: red;
+    width: 20px;
+    height: 20px;
+    margin-left: 215px;
+    margin-top: 4px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+h4 {
+    color: aliceblue;
+}
+
+.pointeur {
+    cursor: grab;
+}
+
+.flex {
+    display: flex;
+}
+
+.style {
+    color: black;
+    font-family: ABeeZee;
+    text-decoration: none;
+    filter: drop-shadow(10px 5px 7px rgba(0, 0, 0, 0.25));
+}
+
+.taille {
+    width: 15px;
+    margin-bottom: -3px;
+
+}
+</style>    
